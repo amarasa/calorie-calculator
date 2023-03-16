48348ffc7ac00d95d87a29aa18064108eea4a842
amarasa <lpangm03@yahoo.com>
2023-03-16 08:04:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -171,15 +171,17 @@ document.getElementById("bodyWeightPlannerForm").addEventListener("submit", (e) 
 
     function calculateBMR(gender, age, height, weight) {
       let bmr;
+      const weightInKg = weight * 0.453592;
 
       if (gender === "male") {
-        bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+        bmr = 10 * weightInKg + 6.25 * height - 5 * age + 5;
       } else if (gender === "female") {
-        bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+        bmr = 10 * weightInKg + 6.25 * height - 5 * age - 161;
       }
 
       return bmr;
     }
+
 
     function calculateTDEE(bmr, activity) {
       const activityMultipliers = {
